2eb8264231372e87e5bf9aaf2efc0d748362b965
Sam Mulube <sam@xively.com>
2013-06-25 16:51:59 +0100

    Add HmacUtils helper for generating activation codes for devices.

---
+++ src/test/java/com/xively/client/utils/HmacUtilsTest.java
@@ -0,0 +1,37 @@
+// Copyright (c) 2003-2013, LogMeIn, Inc. All rights reserved.
+// This is part of Xively4J library, it is under the BSD 3-Clause license.
+package com.xively.client.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author sam
+ *
+ */
+public class HmacUtilsTest {
+
+	@Test
+	public void generatingHmac() {
+		String key = "377f93e5649c31635191c0e22258f5a5744698d3";
+		String value = "0001";
+		String hmac = "13bf6065634a624104cfa14a1f977bede2aa39d8";
+
+		assertEquals(hmac, HmacUtils.hmacSha1(key, value));
+	}
+
+	@Test
+	public void generatingHmacViaGeneralMethod() {
+		String key = "377f93e5649c31635191c0e22258f5a5744698d3";
+		String value = "0001";
+		String hmac = "13bf6065634a624104cfa14a1f977bede2aa39d8";
+
+		assertEquals(hmac, HmacUtils.hmacSha("HmacSHA1", key, value));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void invalidCrypto() {
+		HmacUtils.hmacSha("foo", "key", "value");
+	}
+}
