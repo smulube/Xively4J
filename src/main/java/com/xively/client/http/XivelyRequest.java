356cb267fb28e3291b46107fd667236f20a76936
Sam Mulube <sam@xively.com>
2013-06-28 01:15:27 +0100

    Allow setting params on XivelyRequest instances.

--- src/main/java/com/xively/client/http/XivelyRequest.java
+++ src/main/java/com/xively/client/http/XivelyRequest.java
@@ -2,11 +2,19 @@
 // This is part of Xively4J library, it is under the BSD 3-Clause license.
 package com.xively.client.http;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import com.xively.client.XivelyConstants;
 
 /**
  * Request class that contains the URI and parameters of the request as well as
@@ -20,120 +28,216 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  */
 public class XivelyRequest {
 
-	private String uri;
+    /**
+     * URL encode value using UTF-8 charset.
+     *
+     * @param value
+     * @return
+     */
+    private static String encode(String value) {
+        try {
+            return URLEncoder.encode(value, XivelyConstants.CHARSET_UTF8);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	private Type type;
+    private String uri;
 
-	private Object object;
+    private Map<String, String> params;
 
-	/**
-	 *
-	 * @param uri
-	 * @return
-	 */
-	public XivelyRequest setUri(String uri) {
-		this.uri = uri;
-		return this;
-	}
+    private Type type;
 
-	/**
-	 *
-	 * @param sb
-	 * @return
-	 */
-	public XivelyRequest setUri(StringBuilder sb) {
-		return setUri(sb.toString());
-	}
+    private Object object;
 
-	/**
-	 *
-	 * @return
-	 */
-	public String getUri() {
-		return this.uri;
-	}
+    /**
+     * Add a single query parameter to this request.
+     * 
+     * @param key
+     * @param value
+     */
+    public void addParam(String key, String value) {
+        if (this.params == null) {
+            this.params = new HashMap<String, String>();
+        }
 
-	/**
-	 * @param type
-	 *            the type to set
-	 */
-	public XivelyRequest setType(Type type) {
-		this.type = type;
-		return this;
-	}
+        this.params.put(key, value);
+    }
 
-	/**
-	 * @return the type
-	 */
-	public Type getType() {
-		return type;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
 
-	/**
-	 * @param object
-	 */
-	public XivelyRequest setObject(Object object) {
-		this.object = object;
-		return this;
-	}
+        XivelyRequest other = (XivelyRequest) obj;
 
-	/**
-	 * @return the object
-	 */
-	public Object getObject() {
-		return object;
-	}
+        return new EqualsBuilder().append(this.uri, other.uri)
+                .append(this.type, other.type)
+                .append(this.object, other.object).isEquals();
+    }
 
-	/**
-	 * Currently just returns the URI, but will do more when we have to handle
-	 * parameters.
-	 *
-	 * @return
-	 */
-	public String generateUri() {
-		return getUri();
-	}
+    /**
+     * Currently just returns the URI, but will do more when we have to handle
+     * parameters.
+     *
+     * @return
+     */
+    public String generateUri() {
+        if (this.uri == null) {
+            return null;
+        }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this).append("uri", uri)
-				.append("type", type).append("object", object).toString();
-	}
+        StringBuilder uriBuilder = new StringBuilder(this.uri);
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (this == obj) {
-			return true;
-		}
-		if (obj.getClass() != getClass()) {
-			return false;
-		}
+        if (this.params != null && !this.params.isEmpty()) {
+            if (this.uri.indexOf('?') == -1) {
+                uriBuilder.append('?');
+            }
 
-		XivelyRequest other = (XivelyRequest) obj;
+            buildParams(uriBuilder);
+        }
 
-		return new EqualsBuilder().append(this.uri, other.uri).isEquals();
-	}
+        return uriBuilder.toString();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder(57, 67).append(this.uri).toHashCode();
-	}
+    /**
+     * @return the object
+     */
+    public Object getObject() {
+        return this.object;
+    }
+
+    /**
+     * @return the params
+     */
+    public Map<String, String> getParams() {
+        return this.params;
+    }
+
+    /**
+     * @return the type
+     */
+    public Type getType() {
+        return this.type;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getUri() {
+        return this.uri;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(57, 67).append(this.uri).append(this.type)
+                .append(this.object).toHashCode();
+    }
+
+    /**
+     * @param object
+     */
+    public XivelyRequest setObject(Object object) {
+        this.object = object;
+        return this;
+    }
+
+    /**
+     * 
+     * @param params
+     *              the params to set
+     * @return
+     */
+    public XivelyRequest setParams(Map<String, String> params) {
+        this.params = params;
+        return this;
+    }
+
+    /**
+     * @param type
+     *            the type to set
+     */
+    public XivelyRequest setType(Type type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * 
+     * @param uri
+     * @return
+     */
+    public XivelyRequest setUri(String uri) {
+        this.uri = uri;
+        return this;
+    }
+
+    /**
+     * 
+     * @param sb
+     * @return
+     */
+    public XivelyRequest setUri(StringBuilder sb) {
+        return setUri(sb.toString());
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("uri", this.uri)
+                .append("type", this.type).append("object", this.object)
+                .toString();
+    }
+
+    /**
+     * @param paramEntry
+     * @param uriBuilder
+     */
+    private void buildParam(Entry<String, String> paramEntry,
+            StringBuilder uriBuilder) {
+        uriBuilder.append(XivelyRequest.encode(paramEntry.getKey()));
+        uriBuilder.append("=");
+        uriBuilder.append(XivelyRequest.encode(paramEntry.getValue()));
+    }
+
+    /**
+     * Build URL encoded query string out of our params Map.
+     * 
+     * @return
+     */
+    private void buildParams(StringBuilder uriBuilder) {
+        Iterator<Entry<String, String>> iterator = this.params.entrySet()
+                .iterator();
+
+        while (iterator.hasNext()) {
+            Entry<String, String> paramEntry = iterator.next();
+            buildParam(paramEntry, uriBuilder);
+
+            if (iterator.hasNext()) {
+                uriBuilder.append("&");
+            }
+        }
+    }
 }
--- src/test/java/com/xively/client/http/XivelyRequestTest.java
+++ src/test/java/com/xively/client/http/XivelyRequestTest.java
@@ -5,6 +5,9 @@ package com.xively.client.http;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,74 +15,97 @@ import com.xively.client.models.Feed;
 
 /**
  * @author sam
- *
+ * 
  */
 public class XivelyRequestTest {
 
-	private XivelyRequest request;
+    private XivelyRequest request;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-		this.request = new XivelyRequest();
-	}
+    @Test
+    public void addParam() {
+        this.request.setUri("http://example.com");
+        this.request.addParam("limit", "100");
+        this.request.addParam("order", "DESC");
+        assertEquals("http://example.com?limit=100&order=DESC",
+                this.request.generateUri());
+    }
 
-	@Test
-	public void canSetGetUri() {
-		this.request.setUri("http://example.com");
-		assertEquals("http://example.com", this.request.getUri());
-	}
+    @Test
+    public void canSetGetObject() {
+        Feed feed = new Feed();
+        this.request.setObject(feed);
+        assertEquals(feed, this.request.getObject());
+    }
 
-	@Test
-	public void canSetUriAsStringBuilder() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("http://example.com");
-		this.request.setUri(sb);
-		assertEquals("http://example.com", this.request.getUri());
-	}
+    @Test
+    public void canSetGetType() {
+        this.request.setType(Feed.class);
+        assertEquals(Feed.class, this.request.getType());
+    }
 
-	@Test
-	public void canSetGetType() {
-		this.request.setType(Feed.class);
-		assertEquals(Feed.class, this.request.getType());
-	}
+    @Test
+    public void canSetGetUri() {
+        this.request.setUri("http://example.com");
+        assertEquals("http://example.com", this.request.getUri());
+    }
 
-	@Test
-	public void canSetGetObject() {
-		Feed feed = new Feed();
-		this.request.setObject(feed);
-		assertEquals(feed, this.request.getObject());
-	}
+    @Test
+    public void canSetUriAsStringBuilder() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://example.com");
+        this.request.setUri(sb);
+        assertEquals("http://example.com", this.request.getUri());
+    }
 
-	@Test
-	public void returnsUsefulToString() {
-		this.request.setUri("http://example.com");
-		this.request.setType(Feed.class);
-		assertTrue(this.request.toString().matches(".*uri=http://example.com.*"));
-		assertTrue(this.request.toString().matches(".*com.xively.client.models.Feed.*"));
-	}
+    @Test
+    public void equals() {
+        this.request.setUri("https://example.com").setType(Feed.class);
+        XivelyRequest request2 = new XivelyRequest();
+        request2.setUri("https://example.com").setType(Feed.class);
+        assertTrue(this.request.equals(request2));
+    }
 
-	@Test
-	public void generateUri() {
-		this.request.setUri("http://example.com");
-		assertEquals("http://example.com", this.request.generateUri());
-	}
+    @Test
+    public void generateUri() {
+        this.request.setUri("http://example.com");
+        assertEquals("http://example.com", this.request.generateUri());
+    }
 
-	@Test
-	public void equalsMatchesOnUri() {
-		this.request.setUri("https://example.com").setType(Feed.class);
-		XivelyRequest request2 = new XivelyRequest();
-		request2.setUri("https://example.com").setType(Feed.class);
-		assertTrue(this.request.equals(request2));
-	}
+    @Test
+    public void hashcode() {
+        this.request.setUri("https://example.com").setType(Feed.class);
+        XivelyRequest request2 = new XivelyRequest();
+        request2.setUri("https://example.com").setType(Feed.class);
+        assertTrue(this.request.hashCode() == request2.hashCode());
+    }
 
-	@Test
-	public void hashcodeOnUri() {
-		this.request.setUri("https://example.com").setType(Feed.class);
-		XivelyRequest request2 = new XivelyRequest();
-		request2.setUri("https://example.com").setType(Feed.class);
-		assertTrue(this.request.hashCode() == request2.hashCode());
-	}
+    @Test
+    public void returnsUsefulToString() {
+        this.request.setUri("http://example.com");
+        this.request.setType(Feed.class);
+        assertTrue(this.request.toString()
+                .matches(".*uri=http://example.com.*"));
+        assertTrue(this.request.toString().matches(
+                ".*com.xively.client.models.Feed.*"));
+    }
+
+    @Test
+    public void setParams() {
+        this.request.setUri("http://example.com");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("limit", "100");
+        params.put("order", "ASC");
+        assertEquals(this.request.setParams(params).getParams(), params);
+        this.request.setParams(params);
+        assertEquals("http://example.com?limit=100&order=ASC",
+                this.request.generateUri());
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.request = new XivelyRequest();
+    }
 }
